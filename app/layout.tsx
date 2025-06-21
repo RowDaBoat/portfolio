90e517b54a45c8fd3a7025ef41078bf2621a9633
Row <reardenr@gmail.com>
2025-06-21 16:35:32 -0300

    Set package name and proper description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: "Row's Portfolio",
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  description: 'Projects, skills, and contact info.'
 }
 
 export default function RootLayout({
