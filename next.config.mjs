5ff9bff803caaa7f9b02e84001e07a84889ba38a
Row <reardenr@gmail.com>
2025-06-21 15:27:54 -0300

    Fix deploy workflow, build a static site

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,11 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 }
 
 export default nextConfig
