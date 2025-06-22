c47968eb8717c77de04ff1f6438bc7370ea777ba
Row <reardenr@gmail.com>
2025-06-22 08:59:39 -0300

    Add icon image

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// Utility function to handle basePath for static assets
 export function getAssetPath(path: string): string {
-  // In static export, we need to prepend the basePath
   const basePath = process.env.NODE_ENV === 'production' ? '/portfolio' : ''
   return `${basePath}${path}`
 }
